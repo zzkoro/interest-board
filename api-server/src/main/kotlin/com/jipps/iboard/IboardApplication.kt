78880f15b530f66c1850237923f5af3a7e5f3b08
junemp <junemp@gmail.com>
2022-11-23 01:01:24 +0900

    초기 설정

---
+++ api-server/src/main/kotlin/com/jipps/iboard/IboardApplication.kt
@@ -0,0 +1,11 @@
+package com.jipps.iboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IboardApplication
+
+fun main(args: Array<String>) {
+	runApplication<IboardApplication>(*args)
+}
